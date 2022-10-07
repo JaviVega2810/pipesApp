226ce4bf7242cefd8c1a8c543dd16324b2033158
JaviVega2810 <javiervega2810@gmail.com>
2022-10-07 15:15:42 -0500

    changes

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+//prime NG
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    ButtonModule,
+    CardModule
+  ]
+})
+export class PrimeNgModule { }
