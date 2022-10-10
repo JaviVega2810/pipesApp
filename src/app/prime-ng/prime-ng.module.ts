ca46dd1d9a976cf357f3be961ecbfe1aa23761c2
JaviVega2810 <javiervega2810@gmail.com>
2022-10-10 18:30:35 -0500

    changes until class 142

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 //prime NG
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
 
 
 
@@ -10,7 +11,8 @@ import { CardModule } from 'primeng/card';
   declarations: [],
   exports: [
     ButtonModule,
-    CardModule
+    CardModule,
+    MenubarModule
   ]
 })
 export class PrimeNgModule { }
