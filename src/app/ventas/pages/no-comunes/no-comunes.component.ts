f4ccc6261ef5aa92386f0b57c62824169d04262a
JaviVega2810 <javiervega2810@gmail.com>
2022-10-27 16:04:49 -0500

    end application Pipes

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
   styleUrls: ['./no-comunes.component.css']
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  //i18nSelect
+  nombre: string = 'María';
+  genero: string = 'femenino';
 
-  ngOnInit(): void {
+  invitacionMapa = {
+    'masculino' : 'invitarlo',
+    'femenino' : 'invitarla'
   }
+
+  //i18nPlural
+  clientes: string[] = ['Maria','Pedro','Raúl','Martin','Horacio'];
+  clientesMap = {
+    '=0' : 'no tenemos clientes esperando',
+    '=1' : 'tenemos 1 clientes esperando',
+    '=2' : 'tenemos 2 clientes esperando',
+    'other' : 'tenemos # clientes esperando'
+  }
+
+  cambiarPersona(){
+    this.nombre = 'Javier';
+    this.genero = 'masculino';
+  }
+
+  borrarCliente(){
+    this.clientes.pop();
+  }
+
+  //keyvaluepair
+  persona = {
+    nombre: 'Javier',
+    edad: 35,
+    direccion: 'La loma'
+  }
+
+  heroes = [
+    {
+      nombre: 'Capitan America',
+      edad: '38',
+      universo: 'Marvel'
+    },
+    {
+      nombre: 'Superman',
+      edad: '32',
+      universo: 'DC'
+    }
+
+  ]
+
+  //Async Pipe
+  miObservable = interval(10);
+
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Fin de la promesa');
+    }, 3500);
+  });
 
 }
